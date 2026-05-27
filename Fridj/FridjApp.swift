cf7fa21b9e21032a7346cea4d0a6ab89462617cd
Ardy <ardyjalili@icloud.com>
2026-05-27 13:23:17 +0200

    Initial Commit

---
+++ Fridj/FridjApp.swift
@@ -0,0 +1,17 @@
+//
+//  FridjApp.swift
+//  Fridj
+//
+//  Created by ardy on 2026-05-27.
+//
+
+import SwiftUI
+
+@main
+struct FridjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FridjTests/FridjTests.swift
@@ -0,0 +1,19 @@
+//
+//  FridjTests.swift
+//  FridjTests
+//
+//  Created by ardy on 2026-05-27.
+//
+
+import Testing
+@testable import Fridj
+
+struct FridjTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
